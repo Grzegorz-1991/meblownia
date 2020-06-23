f1baee8d1475f828832166500159cc214b5927a4
Grzegorz Swiecinski <grzegorz.swiecinski@gmail.com>
2020-06-23 22:33:02 +0200

    Gotowa apka przed stylingiem

--- app/controllers/mebles_controller.rb
+++ app/controllers/mebles_controller.rb
@@ -1,7 +1,8 @@
 class MeblesController < ApplicationController
+    before_action :set_meble, only: [:show, :edit, :update, :destroy]
 
     def show
-        @meble = Meble.find(params[:id])
+ 
     end
 
     def index
@@ -9,7 +10,7 @@ class MeblesController < ApplicationController
     end
 
     def create
-        @meble = Meble.new(params.require(:meble).permit(:nazwa,:rodzaj,:opis))
+        @meble = Meble.new(meble_params)
         if @meble.save
             flash[:notice] = "Artykuł poprawnie utworzono!"
             redirect_to @meble
@@ -23,16 +24,32 @@ class MeblesController < ApplicationController
     end
 
     def edit
-        @meble = Meble.find(params[:id])
+       
     end
 
     def update
-        @meble = Meble.find(params[:id])
-        if @meble.update(params.require(:meble).permit(:nazwa,:rodzaj,:opis))
+    
+        if @meble.update(meble_params)
             flash[:notice] = "Poprawnie edytowano artykuł."
             redirect_to @meble
         else
             render 'edit'
         end
+    end
+
+    def destroy
+     
+        @meble.destroy
+        redirect_to mebles_path
+    end
+
+    private
+    
+    def set_meble
+        @meble = Meble.find(params[:id])
+    end
+
+    def meble_params
+        params.require(:meble).permit(:nazwa,:rodzaj,:opis)
     end
 end
